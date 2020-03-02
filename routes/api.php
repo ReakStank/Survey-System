cca8ff3460e4426a475459822877694013e11feb
Thaisan UN <mr.thaisan1213@gmail.com>
2020-03-02 09:51:54 +0700

    update version

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('role/get','roleController@index');
+Route::get('role/get','RoleController@index');
